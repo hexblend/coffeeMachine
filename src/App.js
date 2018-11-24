e4b4c6897c2e08fcb4b3c2dbe29f83541120d170
vladbbr <33767184+vladbbr@users.noreply.github.com>
2018-11-24 16:04:46 +0000

    Basic component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './Assets/scss/main.scss';
 
+import Landing from './Components/Sections/Landing';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <h1>Hello World</h1>
-            </div>
-          </div>
-        </div>
+        <Landing />
       </div>
     );
   }
---
+++ src/Components/Sections/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <section className="landing-section">
+            <h1>Landing Section</h1>
+        </section>
+    );
+}
+
+export default Landing;
