caaaef9bcae624a530cdba4cbd47e08e79c36677
vladbbr <33767184+vladbbr@users.noreply.github.com>
2018-11-24 18:05:14 +0000

    responsive background set + some JSX

--- src/Components/Sections/Landing.js
+++ src/Components/Sections/Landing.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import logo from '../../Assets/img/logo.png';
 
 const Landing = () => {
     return (
         <section className="landing-section">
-            Landing Section
+            <div className="container">
+                <div className="row">
+                    <div className="middle-text">
+                        <div className="col-12">
+                            <img src={logo} alt="Company logo"/>
+                            <h1>Passionata 20</h1>
+                        </div>
+                        <div className="col-12">
+                            <h2>Passion showed in every coffee.</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
