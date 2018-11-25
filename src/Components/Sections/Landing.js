fa01c1fc33576f797cfddfab7373838957718178
vladbbr <33767184+vladbbr@users.noreply.github.com>
2018-11-25 13:16:03 +0000

    Desktop styling done based on grid

--- src/Components/Sections/Landing.js
+++ src/Components/Sections/Landing.js
@@ -2,23 +2,25 @@ import React from 'react';
 import logo from '../../Assets/img/logo.png';
 
 const Landing = () => {
-    return (
-        <section className="landing-section">
-            <div className="container">
-                <div className="row">
-                    <div className="middle-text">
-                        <div className="col-12">
-                            <img src={logo} alt="Company logo"/>
-                            <h1>Passionata 20</h1>
-                        </div>
-                        <div className="col-12">
-                            <h2>Passion showed in every coffee.</h2>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="landing-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <img src={logo} alt="Company logo" />
+            <h1>Passionata 20</h1>
+          </div>
+          <div className="col-12">
+            <h2>Passion showed in every coffee.</h2>
+          </div>
+          <div className="col-12">
+            <a href="#" className="transparent-button">Explore</a>
+          </div>
+        </div>
+        <span className="arrow"><i class="fas fa-caret-down"></i></span>
+      </div>
+    </section>
+  );
 }
 
 export default Landing;
