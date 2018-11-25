690c449b095bfe7db45a33d8406bf75fe264aeed
vladbbr <33767184+vladbbr@users.noreply.github.com>
2018-11-25 15:11:45 +0000

    page responsiveness

--- src/Components/Sections/Landing.js
+++ src/Components/Sections/Landing.js
@@ -3,22 +3,20 @@ import logo from '../../Assets/img/logo.png';
 
 const Landing = () => {
   return (
-    <section className="landing-section">
+    <section className="landing-section" id="landing">
       <div className="container">
         <div className="row">
-          <div className="col-12">
-            <img src={logo} alt="Company logo" />
-            <h1>Passionata 20</h1>
-          </div>
-          <div className="col-12">
+          <div className="col-12 middle-elements">
+            <div className="logo">
+              <img src={logo} alt="Company logo" />
+              <h1>Passionata 20</h1>
+            </div>
             <h2>Passion showed in every coffee.</h2>
-          </div>
-          <div className="col-12">
             <a href="#" className="transparent-button">Explore</a>
-          </div>
-        </div>
+          </div> {/* end of .col-12 */}
+        </div> {/* end of .row */}
         <span className="arrow"><i class="fas fa-caret-down"></i></span>
-      </div>
+      </div> {/* end of .container */}
     </section>
   );
 }
