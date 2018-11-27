07c21135351178e8d30337f595f0188ede064d0b
Vlad Bibire <33767184+webexplained@users.noreply.github.com>
2018-11-27 23:39:25 +0000

    Buy Section Layout Done for all platforms

--- src/Components/Sections/Buy.js
+++ src/Components/Sections/Buy.js
@@ -10,8 +10,8 @@ const Buy = () => {
         <h2>Fancy to have it? See the options.</h2>
         <div className="row">
           <div className="col-6">
-            <img src={amazonBtn} alt="Amazon Buy Button"/>
-            <img src={storeBtn} alt="Official Store Buy Button"/>
+            <a href="https://www.amazon.co.uk/Klarstein-Passionata-Espresso-Capuccino-Preparing/dp/B077B3Z8SY/ref=sr_1_2?ie=UTF8&qid=1543360763&sr=8-2&keywords=klarstein+passionata"><img src={amazonBtn} alt="Amazon Buy Button" /></a>
+            <a href="https://www.klarstein.co.uk/Kitchen-appliances/Coffee/Coffee-machines/Passionata-20-Espresso-Machine-20-Bar-Capuccino-Milk-Foam-Silver-Silver.html?utm_source=connexity&utm_medium=psm&utm_term=10031702&utm_campaign=Kitchen%20appliances"><img src={storeBtn} alt="Official Store Buy Button" /></a>
           </div>
           <div className="col-6">
             <img src={machinePic} alt="Coffe Machine 6"/>
