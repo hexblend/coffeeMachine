d3e500be4a52ba60defde7c78e4946304a31a0fa
Vlad Bibire <33767184+webexplained@users.noreply.github.com>
2018-11-30 01:41:14 +0000

    Navbar Styling + Animation When User Starts Scrolling

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import ScrollspyNav from 'react-scrollspy-nav';
+import logo from '../../Assets/img/logo-nav.png';
 
-class Navbar extends React.Component {
+class Navbar extends Component {
+  // Check if user has started to scroll
+  state = {
+    isTop: true
+  };
+
+  componentDidMount(){
+    document.addEventListener('scroll', () =>{
+      const isTop = window.scrollY < 100;
+      if(isTop !== this.state.isTop) {
+        this.setState({isTop})
+      }
+    });
+  }
+
   render() {
     return (
       <div>
@@ -11,14 +26,28 @@ class Navbar extends React.Component {
           scrollDuration="650"
           headerBackground="true"
         >
-          <ul>
-            <li><a href="/"><span>Home</span></a></li>
-            <li><a href="#about"><span>Description</span></a></li>
-            <li><a href="#features"><span>Top Features</span></a></li>
-            <li><a href="#details"><span>Technical Details</span></a></li>
-            <li><a href="#included"><span>What's Included</span></a></li>
-            <li><a href="#buy"><span>Buy</span></a></li>
-          </ul>
+          {/* ES6 Styling Depending on the navbar position */}
+          <nav style={this.state.isTop ? { background: 'transparent' } : 
+                    { background: '#181818',
+                      position: 'fixed',
+                      top: 0
+                    }}>
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <a href="/" className="logo"><img src={logo} alt="logo" /></a>
+                  <ul>
+                    <li><a href="/"><span>Home</span></a></li>
+                    <li><a href="#about"><span>Description</span></a></li>
+                    <li><a href="#features"><span>Top features</span></a></li>
+                    <li><a href="#details"><span>Technical details</span></a></li>
+                    <li><a href="#included"><span>What's included</span></a></li>
+                    <li><a href="#buy"><span>Buy</span></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </nav>
         </ScrollspyNav>
       </div>
     );
