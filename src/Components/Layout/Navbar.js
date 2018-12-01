b1e47615ffbac32881cf8a5f491e3e6658dbdf60
Vlad Bibire <33767184+webexplained@users.noreply.github.com>
2018-12-01 00:21:25 +0000

    Navbar Closing Functionality after clicking on a mobile link

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -3,11 +3,13 @@ import ScrollspyNav from 'react-scrollspy-nav';
 import logo from '../../Assets/img/logo-nav.png';
 
 class Navbar extends Component {
+	// Initial State
 	state = {
 		isTop: true,
 		isOpened: false
 	};
 
+	// Check if user have started scrolling
   componentDidMount(){
     document.addEventListener('scroll', () =>{
       const isTop = window.scrollY < 100;
@@ -17,6 +19,7 @@ class Navbar extends Component {
 		});
 	}
 
+	// Change state when mobile nav button is clicked
 	toggleNav = () => {
 		this.setState({
 			isOpened: !this.state.isOpened
@@ -32,6 +35,7 @@ class Navbar extends Component {
           scrollDuration="650"
           headerBackground="true"
         >
+					{/* Custom Styling if the user have started scrolling */}
 					<nav 
 						style={this.state.isTop ? 
 									{background: 'transparent'} : 
@@ -41,6 +45,7 @@ class Navbar extends Component {
 									 height: 45,
 									 border: 'none'
 									}}
+						// If user clicked the nav button toggle class .mobile-nav-open
 						className={this.state.isOpened ? 'mobile-nav-open' : ''}>
 
             <div className="container">
@@ -52,13 +57,15 @@ class Navbar extends Component {
 										</span>
 										<a href="/" className="logo"><img src={logo} alt="logo" /></a>
 									</div>
+									{/* If user clicked the nav button toggle class .mobile-links-visible */}
                   <ul className={this.state.isOpened ? 'links mobile-links-visible' : 'links'}>
-                    <li><a href="#landing">Home</a></li>
-                    <li><a href="#about">Description</a></li>
-                    <li><a href="#features">Top features</a></li>
-                    <li><a href="#details">Technical details</a></li>
-                    <li><a href="#included">What's included</a></li>
-                    <li><a href="#buy">Buy</a></li>
+										{/* onClick event on every link to close the navbar after click */}
+                    <li><a href="#landing" onClick={this.toggleNav}>Home</a></li>
+										<li><a href="#about" onClick={this.toggleNav}>Description</a></li>
+										<li><a href="#features" onClick={this.toggleNav}>Top features</a></li>
+										<li><a href="#details" onClick={this.toggleNav}>Technical details</a></li>
+										<li><a href="#included" onClick={this.toggleNav}>What's included</a></li>
+										<li><a href="#buy" onClick={this.toggleNav}>Buy</a></li>
                   </ul>
                 </div> {/* end of .col-12 */}
 							</div> {/* end of .row */}
