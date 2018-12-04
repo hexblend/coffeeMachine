c66d196e6302e39a95294a282c539d11c1e1ef46
Vlad Bibire <33767184+webexplained@users.noreply.github.com>
2018-12-04 13:26:13 +0000

    Updates

---
+++ precache-manifest.dd45b6ce37598bfe56dc8e058380d613.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f31eec12c82e415778fc9a77621da25",
+    "url": "/coffeeMachine/static/media/details_machine.6f31eec1.png"
+  },
+  {
+    "revision": "b7ce8170cc8d70ac7c8f",
+    "url": "/coffeeMachine/static/css/main.9f14ea87.chunk.css"
+  },
+  {
+    "revision": "a021895d071440c20fa1",
+    "url": "/coffeeMachine/static/js/1.a021895d.chunk.js"
+  },
+  {
+    "revision": "1054ce444b88a5c35add",
+    "url": "/coffeeMachine/static/js/runtime~main.1054ce44.js"
+  },
+  {
+    "revision": "3e7c20fcedc6c0ac918a51cbf7603db6",
+    "url": "/coffeeMachine/static/media/description_machine.3e7c20fc.jpg"
+  },
+  {
+    "revision": "f1d8ed2bec31922550fd31cf80ec5e31",
+    "url": "/coffeeMachine/static/media/features_machine.f1d8ed2b.png"
+  },
+  {
+    "revision": "b7ce8170cc8d70ac7c8f",
+    "url": "/coffeeMachine/static/js/main.b7ce8170.chunk.js"
+  },
+  {
+    "revision": "a246c4d931a8e3940e1a504ed740d628",
+    "url": "/coffeeMachine/static/media/amazon-buy-btn.a246c4d9.png"
+  },
+  {
+    "revision": "b278fd4500ebf4cf243fcf2ff6ffd209",
+    "url": "/coffeeMachine/static/media/buy_machine.b278fd45.jpg"
+  },
+  {
+    "revision": "f83d96e93a17f0303fd5473448e95cac",
+    "url": "/coffeeMachine/static/media/landing_background.f83d96e9.jpg"
+  },
+  {
+    "revision": "7b81d046e6ef82650d1bb41a29567529",
+    "url": "/coffeeMachine/static/media/whats_included_bg.7b81d046.png"
+  },
+  {
+    "revision": "16e79a0c75aaea7147a8b639c1a03618",
+    "url": "/coffeeMachine/static/media/whats_included_machine_mobile.16e79a0c.png"
+  },
+  {
+    "revision": "a9bc5a1a00c5c6c84d4dd9b074bc4ccb",
+    "url": "/coffeeMachine/index.html"
+  }
+];
