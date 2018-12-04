456940ab846f966048134e668fbdae3ded6e2cef
Vlad Bibire <33767184+webexplained@users.noreply.github.com>
2018-12-04 13:18:17 +0000

    Final tweaks

--- src/Components/Sections/Buy.js
+++ src/Components/Sections/Buy.js
@@ -11,8 +11,8 @@ const Buy = () => {
         <h2>Fancy to have it? See the options.</h2>
         <div className="row">
           <div className="col-6">
-            <a href="https://www.amazon.co.uk/Klarstein-Passionata-Espresso-Capuccino-Preparing/dp/B077B3Z8SY/ref=sr_1_2?ie=UTF8&qid=1543360763&sr=8-2&keywords=klarstein+passionata"><img src={amazonBtn} alt="Amazon Buy Button" /></a>
-            <a href="https://www.klarstein.co.uk/Kitchen-appliances/Coffee/Coffee-machines/Passionata-20-Espresso-Machine-20-Bar-Capuccino-Milk-Foam-Silver-Silver.html?utm_source=connexity&utm_medium=psm&utm_term=10031702&utm_campaign=Kitchen%20appliances"><img src={storeBtn} alt="Official Store Buy Button" /></a>
+            <a href="https://www.amazon.co.uk/Klarstein-Passionata-Espresso-Capuccino-Preparing/dp/B077B3Z8SY/ref=sr_1_2?ie=UTF8&qid=1543360763&sr=8-2&keywords=klarstein+passionata" target="_blank" rel="noopener noreferrer"><img src={amazonBtn} alt="Amazon Buy Button" /></a>
+            <a href="https://www.klarstein.co.uk/Kitchen-appliances/Coffee/Coffee-machines/Passionata-20-Espresso-Machine-20-Bar-Capuccino-Milk-Foam-Silver-Silver.html?utm_source=connexity&utm_medium=psm&utm_term=10031702&utm_campaign=Kitchen%20appliances" target="_blank" rel="noopener noreferrer"><img src={storeBtn} alt="Official Store Buy Button"/></a>
           </div>
           <div className="col-6">
             <Fade right>
